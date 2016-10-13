6c20094b93f8bc1950a2f9727e52e73ed92f6f26
默司 <mosluce@gmail.com>
2016-10-13 13:35:40 +0800

    chore(files): remove not used files

--- typings/index.d.ts
+++
@@ -1,5 +0,0 @@
-/// <reference path="globals/html-webpack-plugin/index.d.ts" />
-/// <reference path="globals/node/index.d.ts" />
-/// <reference path="modules/lodash/index.d.ts" />
-/// <reference path="modules/mongoose/index.d.ts" />
-/// <reference path="modules/webpack/index.d.ts" />
